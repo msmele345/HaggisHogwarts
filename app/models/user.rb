a11a476e9545be97c3605cdb8082051158e98db8
Adam Johnson & Kalier Howard <pair+adamjohnsonnnn.kaynewbie1@devbootcamp.com>
2017-11-22 16:17:09 -0600

    registration and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
 
+  has_secure_password
+
 	has_many :recipes, foreign_key: :chef_id
 	has_many :categories, through: :recipes
+
+  validates :first_name, :last_name, :email, presence: true
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:create, :show, :new]
+
+  resources :categories do
+    resources :recipes
+  end
+
+  resources :sessions
+
+  root "categories#index"
+
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 end
